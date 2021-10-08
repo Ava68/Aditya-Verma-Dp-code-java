39a12e1a831f77d0132af79eec8fca356cd17a7b
Avadhesh kumar singh <63304698+Ava68@users.noreply.github.com>
2021-10-08 16:19:12 +0530

    Create LongestPalindromicSubseq.java

---
+++ LongestPalindromicSubseq.java
@@ -0,0 +1,28 @@
+// problem link -> https://leetcode.com/problems/longest-palindromic-subsequence/submissions/
+// lcs pattern
+
+public int longestPalindromeSubseq(String s) {
+        String rev = "";
+        
+        for(int i =s.length()-1; i>=0; i--)
+            rev += s.charAt(i);
+        
+        int m = s.length();
+        int n = rev.length();
+        
+        int dp[][] = new int[m+1][n+1];
+        
+        for(int i =1; i<=m; i++)
+        {
+            for(int j =1; j<=n; j++)
+            {
+                if(s.charAt(i-1) == rev.charAt(j-1))
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                
+            }
+        }
+        
+        return dp[m][n];
+    }
